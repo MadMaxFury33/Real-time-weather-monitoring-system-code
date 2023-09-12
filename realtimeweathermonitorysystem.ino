bd92f59f4d62424541ea5b939fcd685e3339d2f3
PrajwalMahanawar <142044643+MadMaxFury33@users.noreply.github.com>
2023-09-12 19:51:32 +0530

    Add files via upload

---
+++ realtimeweathermonitorysystem.ino
@@ -0,0 +1,175 @@
+#include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
+#define DHTTYPE DHT11   // DHT 11
+
+#define dht_dpin 12
+DHT dht(dht_dpin, DHTTYPE);
+
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+
+int rainsensorpin = 13;
+
+SFE_BMP180 pressure;
+WiFiClient client;
+
+#define ALTITUDE 1655.0 // Altitude in meters
+
+String apiKey = "FKJET83YUM1QOG26";
+const char *ssid =  "V2031";
+const char *pass =  "prajwal33";
+const char* server = "api.thingspeak.com";
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(rainsensorpin, INPUT);
+  dht.begin();
+
+  Serial.begin(9600);
+   Wire.begin();
+   
+  WiFi.begin(ssid, pass);
+
+  if (pressure.begin())
+    Serial.println("BMP180 init success");
+  else
+  {
+    Serial.println("BMP180 init fail\n\n");
+    while (1); // Pause forever.
+  }
+  
+   while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float h = dht.readHumidity();
+  delay(500);
+  float t = dht.readTemperature();
+  delay(500);
+  Serial.println();
+  Serial.print("Current humidity = ");
+  Serial.print(h);
+  Serial.print("%  ");
+  Serial.print("temperature = ");
+  Serial.print(t);
+  Serial.println("C  ");
+  Serial.println();
+  delay(1000);
+
+  int value = digitalRead(rainsensorpin);
+  if (value == LOW)
+  {
+    Serial.println("ITS RAINING TODAY");
+    Serial.println();
+    delay(2000);
+  }
+  if (value == HIGH)
+  {
+    Serial.println("ITS NOT RAINING");
+    Serial.println();
+    delay(2000);
+  }
+
+  char status;
+  double T, P, p0, a;
+
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Print out the measurement:
+      Serial.print("temperature: ");
+      Serial.print(T,2);
+      Serial.print(" deg C, ");
+      Serial.print((9.0/5.0)*T+32.0,2);
+      Serial.println(" deg F");
+      
+     
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // Print out the measurement:
+          Serial.print("absolute pressure: ");
+          Serial.print(P,2);
+          Serial.print(" mb, ");
+          Serial.print(P*0.0295333727,2);
+          Serial.println(" inHg");
+
+          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+          Serial.print("relative (sea-level) pressure: ");
+          Serial.print(p0,2);
+          Serial.print(" mb, ");
+          Serial.print(p0*0.0295333727,2);
+          Serial.println(" inHg");
+
+         a = pressure.altitude(P,p0);
+          Serial.print("computed altitude: ");
+          Serial.print(a,0);
+          Serial.print(" meters, ");
+          Serial.print(a*3.28084,0);
+          Serial.println(" feet");
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+ if (client.connect(server, 80)) {
+    String postStr = apiKey;
+    postStr += "&field1=";
+    postStr += String(t);
+    postStr += "&field2=";
+    postStr += String(h);
+    postStr += "&field3=";
+    postStr += String(P, 2);
+    postStr += "&field4=";
+    postStr += String(value);
+    postStr += "\r\n\r\n\r\n\r\n";
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n\n\n");
+    client.print(postStr);
+
+    Serial.print("Temperature: ");
+    Serial.println(t);
+    Serial.print("Humidity: ");
+    Serial.println(h);
+    Serial.print("absolute pressure: ");
+    Serial.print(P, 2);
+    Serial.println("mb");
+    Serial.print("Rain");
+    Serial.println(value);
+
+  } 
+  client.stop();
+  delay(100);
+}
